65adf7b11c0605e24b0185fe876cc4962a21115a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-12 01:16:39 -0300

    SPRITE_*_OFFSET.

--- eac-ca65-nes-lib/nes/sprites.s
+++ eac-ca65-nes-lib/nes/sprites.s
@@ -1,7 +1,12 @@
 ; https://wiki.nesdev.org/w/index.php?title=PPU_OAM
 
+SPRITE_Y_OFFSET = 0
+SPRITE_T_OFFSET = 1
+SPRITE_A_OFFSET = 2
+SPRITE_X_OFFSET = 3
+
 .define sprite_address(sprite_index, byte_index) oam + (sprite_index * 4) + byte_index
-.define sprite_y(sprite_index) sprite_address(sprite_index, 0)
-.define sprite_t(sprite_index) sprite_address(sprite_index, 1)
-.define sprite_a(sprite_index) sprite_address(sprite_index, 2)
-.define sprite_x(sprite_index) sprite_address(sprite_index, 3)
+.define sprite_y(sprite_index) sprite_address(sprite_index, SPRITE_Y_OFFSET)
+.define sprite_t(sprite_index) sprite_address(sprite_index, SPRITE_T_OFFSET)
+.define sprite_a(sprite_index) sprite_address(sprite_index, SPRITE_A_OFFSET)
+.define sprite_x(sprite_index) sprite_address(sprite_index, SPRITE_X_OFFSET)
