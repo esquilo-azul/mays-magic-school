786bb0322e681d56aad47331a5475ef65528951e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-13 00:50:13 -0300

    sprite_t: rename from sprite_i.

--- eac-ca65-nes-lib/nes/sprites.s
+++ eac-ca65-nes-lib/nes/sprites.s
@@ -2,6 +2,6 @@
 
 .define sprite_address(sprite_index, byte_index) oam + (sprite_index * 4) + byte_index
 .define sprite_y(sprite_index) sprite_address(sprite_index, 0)
-.define sprite_i(sprite_index) sprite_address(sprite_index, 1)
+.define sprite_t(sprite_index) sprite_address(sprite_index, 1)
 .define sprite_a(sprite_index) sprite_address(sprite_index, 2)
 .define sprite_x(sprite_index) sprite_address(sprite_index, 3)
