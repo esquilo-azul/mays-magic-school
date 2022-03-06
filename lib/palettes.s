9bfd5ec1fe9c2528fa1143a267b1f42b40a6c60e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-06 10:59:01 -0300

    lib/palettes.s: as from tiles plan.

--- lib/palettes.s
+++ lib/palettes.s
@@ -1,8 +1,8 @@
-.byte $0F,$15,$26,$37 ; bg0 purple/pink
-.byte $0F,$09,$19,$29 ; bg1 green
-.byte $0F,$01,$11,$21 ; bg2 blue
-.byte $0F,$00,$10,$30 ; bg3 greyscale
-.byte $0F,$18,$28,$38 ; sp0 yellow
-.byte $0F,$14,$24,$34 ; sp1 purple
-.byte $0F,$1B,$2B,$3B ; sp2 teal
-.byte $0F,$12,$22,$32 ; sp3 marine
+.byte $0F, $15, $26, $37; bg0_purple_pink: black_e2_dd, red_d, brown_l, orange_ll
+.byte $0F, $09, $19, $29; bg1_green: black_e2_dd, lawn_green_dd, lawn_green_d, lawn_green_l
+.byte $0F, $01, $11, $21; bg2_blue: black_e2_dd, blue_dd, blue_d, blue_l
+.byte $0F, $00, $10, $30; bg3_greyscale: black_e2_dd, grey_dd, grey_d, grey_ll
+.byte $0F, $18, $28, $38; sp0_yellow: black_e2_dd, yellow_d, yellow_l, yellow_ll
+.byte $0F, $14, $24, $34; sp1_purple: black_e2_dd, violet_d, violet_l, violet_ll
+.byte $0F, $1B, $2B, $3B; sp2_teal: black_e2_dd, sea_green_d, sea_green_l, sea_green_ll
+.byte $0F, $12, $22, $32; sp3_marine: black_e2_dd, indigo_d, indigo_l, indigo_ll
