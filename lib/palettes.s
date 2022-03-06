aa579b7c74937e5943c1f236402378d6cd02d7ac
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-06 10:16:37 -0300

    lib/palettes.s: extract from "main.s".

---
+++ lib/palettes.s
@@ -0,0 +1,8 @@
+.byte $0F,$15,$26,$37 ; bg0 purple/pink
+.byte $0F,$09,$19,$29 ; bg1 green
+.byte $0F,$01,$11,$21 ; bg2 blue
+.byte $0F,$00,$10,$30 ; bg3 greyscale
+.byte $0F,$18,$28,$38 ; sp0 yellow
+.byte $0F,$14,$24,$34 ; sp1 purple
+.byte $0F,$1B,$2B,$3B ; sp2 teal
+.byte $0F,$12,$22,$32 ; sp3 marine
