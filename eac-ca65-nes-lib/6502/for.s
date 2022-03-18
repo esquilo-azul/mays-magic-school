46537251ebc4e538f86418a7de1d1d4714c9fbb1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 11:26:09 -0300

    for_y_*.

--- eac-ca65-nes-lib/6502/for.s
+++ eac-ca65-nes-lib/6502/for.s
@@ -22,3 +22,28 @@
   dex
   bne :-
 .endmacro
+
+.macro for_y_asc_start start_count
+  .ifblank start_count
+    ldy #0
+  .else
+    ldy start_count
+  .endif
+  :
+.endmacro
+
+.macro for_y_asc_end count
+  iny
+  cpy count
+  bcc :--
+.endmacro
+
+.macro for_y_desc_start count
+  ldy count
+  :
+.endmacro
+
+.macro for_y_desc_end
+  dey
+  bne :--
+.endmacro
