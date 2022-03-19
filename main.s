9f36633327c722b3d39ea82c6c9b01db30c303a5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-19 00:10:30 -0300

    main.s: remove comments.

--- main.s
+++ main.s
@@ -1,42 +1,3 @@
-;
-; example.s
-; Brad Smith (rainwarrior), 4/06/2014
-; http://rainwarrior.ca
-;
-; This is intended as an introductory example to NES programming with ca65.
-; It covers the basic use of background, sprites, and the controller.
-; This does not demonstrate how to use sound.
-;
-; This is not intended as a ready-made game. It is only a very minimal
-; playground to assist getting started in NES programming. The idea here is
-; to get you past the most difficult parts of a minimal NES program setup
-; so that you can experiment from an almost blank slate.
-;
-; To use your own graphics, replace the two 4k tile banks provided.
-; They are named "background.chr" and "sprite.chr".
-;
-; The reset and nmi routines are provided as a simple working example of
-; these things. Writing these from scratch is a more advanced topic, so they
-; will not be fully explained here.
-;
-; Under "drawing utilities" are some very primitive routines for working
-; with the NES graphics. See the "main" section for examples of how to use them.
-;
-; Finally at the bottom you will find the "main" section that provides
-; a small example program. A cursor is shown. Pressing the d-pad will move
-;   - pressing the d-pad will move the cursor around the screen
-;   - pressing B will draw a tile to the screen
-;   - pressing A will draw several tiles to the screen
-;   - pressing SELECT will reset the background
-;   - holding START will demonstrate scrolling
-;
-; Please note that this example code is intended to be simple, not necessarily
-; efficient. I have tried to avoid optimization in favour of easier to understand code.
-;
-; You may notice some odd behaviour when using the A button around the edges of the screen.
-; I will leave it as an exercise for the curious to understand what is going on.
-;
-
 .include "eac-ca65-nes-lib/main.s"
 .include "lib/memory_map.s"
 .include "lib/palettes.s"
@@ -50,7 +11,3 @@
 .include "lib/characters.s"
 .include "lib/vectors.s"
 .include "lib/main.s"
-
-;
-; end of file
-;
