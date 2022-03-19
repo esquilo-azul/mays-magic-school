7989af53714f8000ea11262409b5d44064920963
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 23:26:06 -0300

    INES_MAPPER_*.

--- eac-ca65-nes-lib/nes/memory_map/ines.s
+++ eac-ca65-nes-lib/nes/memory_map/ines.s
@@ -1,5 +1,9 @@
 ; Reference: https://www.nesdev.org/wiki/INES#iNES_file_format
 
-INES_MAPPER = 0 ; 0 = NROM
+; Others: https://www.nesdev.org/wiki/Mapper#iNES_1.0_mapper_grid
+INES_MAPPER_NROM = 0
+INES_MAPPER_DEFAULT = INES_MAPPER_NROM
+
+INES_MAPPER = INES_MAPPER_DEFAULT
 INES_MIRROR = 1 ; 0 = horizontal mirroring, 1 = vertical mirroring
 INES_SRAM = 0 ; 1 = battery backed SRAM at $6000-7FFF
