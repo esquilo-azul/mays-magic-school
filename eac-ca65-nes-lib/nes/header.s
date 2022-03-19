8df232aecd138633b55091489fd6e960886d5dc3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 23:40:31 -0300

    nes_header: SRAM presence configurable.

--- eac-ca65-nes-lib/nes/header.s
+++ eac-ca65-nes-lib/nes/header.s
@@ -1,4 +1,4 @@
-.macro nes_header mapper, mirror
+.macro nes_header mapper, mirror, sram
   .ifnblank mapper
     v_mapper .set mapper
   .else
@@ -9,12 +9,17 @@
   .else
     v_mirror .set INES_MIRROR_DEFAULT
   .endif
+  .ifnblank sram
+    v_sram .set sram
+  .else
+    v_sram .set INES_SRAM_DEFAULT
+  .endif
   .segment "HEADER"
 
   .byte 'N', 'E', 'S', $1A ; ID
   .byte $02 ; 16k PRG chunk count
   .byte $01 ; 8k CHR chunk count
-  .byte v_mirror | (INES_SRAM << 1) | ((v_mapper & $f) << 4)
+  .byte v_mirror | (v_sram << 1) | ((v_mapper & $f) << 4)
   .byte (v_mapper & %11110000)
   .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
 .endmacro
