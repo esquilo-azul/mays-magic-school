0131a3b4fa7ca43bb8efb92e333a8ecab6e770b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 23:39:29 -0300

    nes_header: mirroring configurable.

--- eac-ca65-nes-lib/nes/header.s
+++ eac-ca65-nes-lib/nes/header.s
@@ -1,15 +1,20 @@
-.macro nes_header mapper
+.macro nes_header mapper, mirror
   .ifnblank mapper
     v_mapper .set mapper
   .else
     v_mapper .set INES_MAPPER_DEFAULT
+  .endif
+  .ifnblank mirror
+    v_mirror .set mirror
+  .else
+    v_mirror .set INES_MIRROR_DEFAULT
   .endif
   .segment "HEADER"
 
   .byte 'N', 'E', 'S', $1A ; ID
   .byte $02 ; 16k PRG chunk count
   .byte $01 ; 8k CHR chunk count
-  .byte INES_MIRROR | (INES_SRAM << 1) | ((v_mapper & $f) << 4)
+  .byte v_mirror | (INES_SRAM << 1) | ((v_mapper & $f) << 4)
   .byte (v_mapper & %11110000)
   .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
 .endmacro
