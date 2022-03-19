8616a077b2c30d6fdb6625d167e25bb67c57ac2c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 22:50:32 -0300

    nes_header: move to "nes/header.s".

--- eac-ca65-nes-lib/nes.s
+++ eac-ca65-nes-lib/nes.s
@@ -5,15 +5,7 @@
 .include "nes/palettes.s"
 .include "nes/sprites.s"
 .include "nes/gamepad.s"
-
-.macro nes_header
-  .byte 'N', 'E', 'S', $1A ; ID
-  .byte $02 ; 16k PRG chunk count
-  .byte $01 ; 8k CHR chunk count
-  .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
-  .byte (INES_MAPPER & %11110000)
-  .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
-.endmacro
+.include "nes/header.s"
 
 .macro nes_reset
   sei       ; mask interrupts
---
+++ eac-ca65-nes-lib/nes/header.s
@@ -0,0 +1,8 @@
+.macro nes_header
+  .byte 'N', 'E', 'S', $1A ; ID
+  .byte $02 ; 16k PRG chunk count
+  .byte $01 ; 8k CHR chunk count
+  .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
+  .byte (INES_MAPPER & %11110000)
+  .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
+.endmacro
