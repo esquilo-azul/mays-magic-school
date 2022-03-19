9677aab0aba02d95f3ca1b2f8cd6c704fb2ad347
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 21:41:57 -0300

    nmi_lock_write: extract from "nmi".

--- lib/vectors/nmi.s
+++ lib/vectors/nmi.s
@@ -1,3 +1,8 @@
+.macro nmi_lock_write lock_value
+  lda #lock_value
+  sta nmi_lock
+.endmacro
+
 .segment "CODE"
 
 nmi:
@@ -7,8 +12,7 @@ nmi:
   beq :+
     jmp @nmi_end
   :
-  lda #1
-  sta nmi_lock
+  nmi_lock_write 1
   ;
   lda ppu_update_status
   bne :+ ; ppu_update_status == 0 not ready to update PPU
@@ -30,7 +34,6 @@ nmi:
 @ppu_update_end:
   ; if this engine had music/sound, this would be a good place to play it
   ; unlock re-entry flag
-  lda #0
-  sta nmi_lock
+  nmi_lock_write 0
 @nmi_end:
   pull_registers
