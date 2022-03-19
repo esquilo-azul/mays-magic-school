4a7f452718ce604162a7f5d66e296c2500196d2f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-03-18 23:38:36 -0300

    nes_header: mapper configurable.

--- eac-ca65-nes-lib/nes/header.s
+++ eac-ca65-nes-lib/nes/header.s
@@ -1,10 +1,15 @@
-.macro nes_header
+.macro nes_header mapper
+  .ifnblank mapper
+    v_mapper .set mapper
+  .else
+    v_mapper .set INES_MAPPER_DEFAULT
+  .endif
   .segment "HEADER"
 
   .byte 'N', 'E', 'S', $1A ; ID
   .byte $02 ; 16k PRG chunk count
   .byte $01 ; 8k CHR chunk count
-  .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
-  .byte (INES_MAPPER & %11110000)
+  .byte INES_MIRROR | (INES_SRAM << 1) | ((v_mapper & $f) << 4)
+  .byte (v_mapper & %11110000)
   .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
 .endmacro
